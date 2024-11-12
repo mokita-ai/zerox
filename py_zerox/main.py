42f2702c3a56fd80a534042a6ef26b217de70abe
mokita-mohammad-dallash <mohammad.dallash@mokita.ai>
2024-11-12 00:22:42 +0000

    max_tokens arg and  migrate with the metrics code

--- py_zerox/main.py
+++ py_zerox/main.py
@@ -7,6 +7,7 @@ from pathlib import Path
 import uuid
 import os
 import json
+import math
 import aioshutil
 import nest_asyncio
 from evaluation_metrics.text_similarity import calculate_rouge_metrics
@@ -17,16 +18,14 @@ from utils.latex_to_json import tex_file_to_json
 from evaluation_metrics.DAR import evaluate_hierarchy
 from utils.heading_normalizer import  normalize_headings
 from dotenv import load_dotenv
-from utils.textblock_extractor import extract_text
-from utils.textblock_extractor import find_and_matching_values
+from utils.block_extractor import extract_text
+from utils.block_extractor import find_and_matching_values
 from utils.common import prepare_fs_examples_pathes
 load_dotenv()
 
 
-# Apply nest_asyncio
 nest_asyncio.apply()
 
-# Required environment variables
 required_env_vars = ["AZURE_API_KEY", "AZURE_API_BASE", "AZURE_API_VERSION"]
 for var in required_env_vars:
     if not os.getenv(var):
@@ -34,11 +33,19 @@ for var in required_env_vars:
 
 
 
+def sanitize_metrics(data):
+    if isinstance(data, dict):
+        return {k: sanitize_metrics(v) for k, v in data.items()}
+    elif isinstance(data, float):
+        return data if math.isfinite(data) else 0.0  # Replace NaN or inf with 0.0
+    else:
+        return data
 
-# Initialize FastAPI app
+
+
+
 app = FastAPI()
 
-# CORS setup
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
@@ -47,7 +54,6 @@ app.add_middleware(
     allow_headers=['*']
 )
 
-# Define root endpoint
 @app.get("/")
 def app_root():
     return {"message": "Hello world"}
@@ -74,13 +80,11 @@ async def pdf_to_latx(
         maintain_format=True,
         select_pages=pages, 
         fewshot_examples_paths=fs_examples,
-        **kwargs  # Unpack the dictionary as keyword arguments
+        **kwargs  
     )
     
    
     return result.pages[0].content
-
-# Endpoint to parse specified pages from uploaded PDF file
 @app.post("/parse-pages")
 async def parse_pages(
     pdf_file: UploadFile, 
@@ -98,9 +102,9 @@ async def parse_pages(
     args = {
         "temperature": 0,
         "top_p": 1,
-        "seed": 42
+        "seed": 42,
+        "max_tokens" : 4096 ,
     }
-
 
     if isinstance(ground_truth_tex_file, str):
         ground_truth_tex_file = None
@@ -122,7 +126,6 @@ async def parse_pages(
             ground_truth_string = await pp_model.cleaning_postprocessing(ground_truth_string , POST_PROCESSING_PROMP )
 
 
-        # Convert the string to a dictionary
         try:
             ground_truth_json = tex_file_to_json(tex_data=ground_truth_string)
         except json.JSONDecodeError:
@@ -151,6 +154,7 @@ async def parse_pages(
             **args  
         )
 
+
         pred_json = tex_file_to_json(tex_data=pred_latex_code)
 
     except Exception as e:
@@ -173,13 +177,17 @@ async def parse_pages(
 
   
 
-    # Get the list of dictionaries with headings and concatenated text
-    pred_text_map = extract_text(updated_pred_json)
-    GT_text_map = extract_text(ground_truth_json)
+
+    pred_text_map, _ = extract_text(updated_pred_json)
+   
+
+
+    GT_text_map, _ = extract_text(ground_truth_json)
+
+   
 
     matching_values = find_and_matching_values(pred_text_map, GT_text_map)
-
-
+    
     rouge_values = calculate_rouge_metrics(matching_values)
     bleu_values = calculate_bleu_metrics(matching_values)
 
@@ -197,82 +205,9 @@ async def parse_pages(
         "bleu_values": bleu_values,
         "hierarchy_metrics": metrics
     }
-                
+
+    metrics =  sanitize_metrics(metrics)
+     
      
     return {'ground_truth_latex': ground_truth_string, 'ground_truth_json': ground_truth_json, 'predicted_latex': pred_latex_code, "predicted_json": pred_json, "metrics": metrics}
-
-
-# class MetricsEvaluationRequest(BaseModel):
-#     prompt: str
-
-
-# @app.post("/metrics-evaluation")
-# async def metrics_evaluation(request: MetricsEvaluationRequest):
-#     prompt = request.prompt
-#     metric_data_files = Path("metric_data_examples")
-
-#     for file_location in metric_data_files.iterdir():
-#         if file_location.suffix == ".pdf":
-#             result  = await zerox(file_path=str(file_location), model=model, output_dir="./output_test",
-#                                           custom_system_prompt=prompt )
-
-#             predicted_latex = ""
-#             for page in result.pages:
-#                 predicted_latex += page.content + '\n'
-                        
-
-#             GT_latex_file = file_location.with_suffix(".tex")
-#             with open(GT_latex_file) as f:
-#                 GT_latex = f.read()
-                
-
-#             predicted_json = tex_file_to_json(tex_data = predicted_latex)
-#             GT_json = tex_file_to_json(tex_data= GT_latex)
-
-
-#             # ##save GT_json
-#             GT_json_file = file_location.with_suffix(".GT.json")
-#             with open(GT_json_file, 'w') as json_file:
-#                 json.dump(GT_json, json_file, indent=4)
-
-#             ##save predicted_json
-#             predicted_json_file = file_location.with_suffix(".predicted.json")
-#             with open(predicted_json_file, 'w') as json_file:
-#                 json.dump(predicted_json, json_file, indent=4)
-
-
-#             updated_pred_json, _ = normalize_headings(predicted_json, GT_json, "logs.txt")
-
-#             ##save updated_pred_json
-#             updated_pred_json_file = file_location.with_suffix(".updated_pred.json")
-#             with open(updated_pred_json_file, 'w') as json_file:
-#                 json.dump(updated_pred_json, json_file, indent=4)
-
-
-#             # Get the list of dictionaries with headings and concatenated text
-#             pred_text_map = extract_text(updated_pred_json)
-#             GT_text_map = extract_text(GT_json)
-
-#             matching_values = find_and_matching_values(pred_text_map, GT_text_map)
-
-
-#             rouge_values = calculate_rouge_metrics(matching_values)
-#             bleu_values = calculate_bleu_metrics(matching_values)
-
-
-
-#             try:
-#                 metrics = evaluate_hierarchy(GT_json, updated_pred_json )
-#             except Exception as e:
-#                 print(f"Error during evaluation: {e}")
-
-
-
-#             response = {
-#                 "rouge_values": rouge_values,
-#                 "bleu_values": bleu_values,
-#                 "hierarchy_metrics": metrics
-#             }
-#             return response
-
 
